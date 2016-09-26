d11545d39cf9dbc0b807923f202262ad75bc08ea
MrBraveChenBinBin <chenbinbin@chenbinbindemacbook.local>
2016-09-26 15:43:02 +0800

    Initial Commit

---
+++ CustomCircularProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCircularProgressView
+//
+//  Created by 彬彬 on 16/9/26.
+//  Copyright © 2016年 陈彬彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomCircularProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCircularProgressView
+//
+//  Created by 彬彬 on 16/9/26.
+//  Copyright © 2016年 陈彬彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
